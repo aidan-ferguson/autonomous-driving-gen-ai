38ca06e90db970d23a406eb48872d296e8106969
Aidan Ferguson <aidan.t.ferguson@gmail.com>
2024-02-21 14:09:04 +0000

    Fix import error

--- eval/diffusion_model.py
+++ eval/diffusion_model.py
@@ -1,4 +1,7 @@
-from ...ldm.ControlNet.share import *
+# Add ControlNet folder to path and import 
+import sys
+import os
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), "../ldm/ControlNet"))
 
 import cv2
 import einops
@@ -6,6 +9,8 @@ import numpy as np
 import torch
 import random
 
+
+from ...ldm.ControlNet.share import *
 from pytorch_lightning import seed_everything
 from ...ldm.ControlNet.cldm.model import create_model, load_state_dict
 from ...ldm.ControlNet.cldm.ddim_hacked import DDIMSampler

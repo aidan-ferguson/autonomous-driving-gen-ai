426354927d1a1b95da6e94fc593fa5b4fc511b98
Aidan Ferguson <aidan.t.ferguson@gmail.com>
2024-02-22 18:12:18 +0000

    evaluation with small bounding boxes removed

--- eval/run_evaluation.py
+++ eval/run_evaluation.py
@@ -30,6 +30,12 @@ os.umask(0o002)
 # For some reason FSOCO images are surrounded by a black border of thickness 140, we remove this
 FSOCO_BORDER = 140
 
+# How small either the width or height of bounding boxes can be and still be included in the training data (in pixels)
+BB_THRESHOLD = 10
+
+# What size of image should the YOLO network accept
+YOLO_INPUT_SIZE = 256
+
 def generate_yolo_yaml(dataset_dir: str):
     return f"""
 path: {dataset_dir}
@@ -105,6 +111,8 @@ def evaluate_diffusion_model(model_path: str, real_world_dir: str, sim_frame_dir
         with open(os.path.join(rw_label_dir, label), "r") as file:
             old_label = [list(map(float, line.split(' '))) for line in file.readlines()]
 
+        excluded_indices = []
+
         # Multiply out by full image size, then re-normalise
         for ann_idx in range(len(old_label)):
             # Change class back to int
@@ -122,6 +130,9 @@ def evaluate_diffusion_model(model_path: str, real_world_dir: str, sim_frame_dir
             width /= img.shape[1]
             height /= img.shape[0]
 
+            if (width*YOLO_INPUT_SIZE < BB_THRESHOLD) or (height*YOLO_INPUT_SIZE < BB_THRESHOLD):
+                excluded_indices.append(ann_idx)
+
             old_label[ann_idx][1] = x1
             old_label[ann_idx][2] = y1
             old_label[ann_idx][3] = width
@@ -129,7 +140,7 @@ def evaluate_diffusion_model(model_path: str, real_world_dir: str, sim_frame_dir
 
         # Now write out new file with corrected annotations
         with open(os.path.join(train_label_dir, label), "w") as file:
-            label = [' '.join(list(map(str, ann))) for ann in old_label]
+            label = [' '.join(list(map(str, ann))) for idx, ann in enumerate(old_label) if idx not in excluded_indices]
             file.write('\n'.join(label))
 
     synthetic_count = 0
@@ -146,10 +157,21 @@ def evaluate_diffusion_model(model_path: str, real_world_dir: str, sim_frame_dir
                 mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2RGB)
                 sample = diffusion_model.forward(mask, n_samples=1)[0]
                 cv2.imwrite(os.path.join(train_image_dir, f"sampled_frame_{sample_idx}.png"), cv2.cvtColor(sample, cv2.COLOR_BGR2RGB))
-                shutil.copyfile(
-                    os.path.join(sim_label_dir, f"frame_{sample_idx}_yolo.txt"),
-                    os.path.join(train_label_dir, f"sampled_frame_{sample_idx}.txt")
-                )
+
+                # Copy over sampled frame bounding boxes but remove bounding boxes with width or height less than some threshold
+                with open(os.path.join(sim_label_dir, f"frame_{sample_idx}_yolo.txt"), "r") as source_file:
+                    with open(os.path.join(train_label_dir, f"sampled_frame_{sample_idx}.txt"), "r") as dest_file:
+                        label = [list(map(float, line.split(' '))) for line in source_file.readlines()]
+                        excluded_indices = []
+                        for idx in range(list(label)):
+                            label[idx][0] = int(label[idx][0])
+                            if (label[idx][3]*YOLO_INPUT_SIZE < BB_THRESHOLD) or (label[idx][4]*YOLO_INPUT_SIZE < BB_THRESHOLD):
+                                excluded_indices.append(idx)
+
+                        label = [row for idx, row in enumerate(label) if idx not in excluded_indices]
+                        dest_file.write('\n'.join([list(map(str, row)) for row in label]))
+
+
             synthetic_count = new_synthetic_count
         
         # YOLO training outputs a folder called 'runs' in the current working dir, so chdir into the iteration
@@ -164,7 +186,10 @@ def evaluate_diffusion_model(model_path: str, real_world_dir: str, sim_frame_dir
         with open(os.path.join(dataset_dir, "fsoco.yaml"), "w") as file:
             file.write(yaml)
 
-        results = model.train(data=os.path.join(dataset_dir, "fsoco.yaml"), epochs=10, imgsz=256, verbose=False)
+        results = model.train(data=os.path.join(dataset_dir, "fsoco.yaml"), epochs=10, imgsz=YOLO_INPUT_SIZE, verbose=False)
+
+        # We want to delete the generated labels.cache so the next iteration will read all the dataset
+        os.remove(os.path.join(dataset_dir, "labels.cache"))
 
 
 def main() -> None:
